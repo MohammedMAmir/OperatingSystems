6e8dec34c1efb8b728d320ad2cbdc6b04f142298
Mohammed Amir <83556373+YaBroMo@users.noreply.github.com>
2022-12-22 18:04:15 -0500

    Add files via upload

---
+++ pipe/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /nfs/ug/homes-0/a/amirmoh4/ece344/pipe
+# Build directory: /nfs/ug/homes-0/a/amirmoh4/ece344/pipe/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
